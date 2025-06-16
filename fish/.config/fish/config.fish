9221243924e4b3d8062cb2820e2007cc03cb3daa
Graskäuppa <birdlandbollas@ciencias.unam.mx>
2025-06-16 18:36:01 +0000

    add dotfiles
    Added dotfiles for Hyprland WM: waybar, wofi, idle, land, lock and
    paper, as well as some additional files for them to use. Also added
    dotfiles for fish, kitty, oh-my-posh and yazi.

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,29 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    set -gx EDITOR nvim
+    # Aliases
+    alias fetch="pfetch"
+    alias paci="sudo pacman -S"
+    alias q="exit"
+    alias l="lsd -l"
+    alias lout="hyprctl dispatch exit"
+
+    # Wrap yazi with y and change cwd when exited
+    function y
+        set tmp (mktemp -t "yazi-cwd.XXXXXX")
+        yazi $argv --cwd-file="$tmp"
+        if read -z cwd <"$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+            builtin cd -- "$cwd"
+        end
+        rm -f -- "$tmp"
+    end
+
+end
+
+# oh-my-posh
+eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+oh-my-posh init fish --config ~/dotfiles/oh-my-posh/catppuccin_mocha.omp.json | source
+
+# zoxide
+zoxide init fish | source
