39b9253cf0aa107c3b533b8fe27fe911d34eaa24
gras <birdlandbollas@ciencias.unam.mx>
2026-04-09 00:50:23 -0600

    added configs for snacks goodies

---
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,21 @@
+return {
+
+  -- lazy.nvim
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      terminal = {
+
+        win = {
+          -- title = "Look into the mirror",
+          -- title_pos = "center",
+          position = "float",
+          border = "rounded",
+          width = 0.5,
+          height = 0.5,
+        },
+      },
+    },
+  },
+}
