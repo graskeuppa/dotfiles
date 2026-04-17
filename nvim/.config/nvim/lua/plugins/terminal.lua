6a54a70289b35bbd6c6448130d1f9c314b554830
gras <birdlandbollas@ciencias.unam.mx>
2026-04-17 06:08:36 -0600

    file cleanup

---
+++ nvim/.config/nvim/lua/plugins/terminal.lua
@@ -0,0 +1,16 @@
+return {
+
+  -- lazy.nvim
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      terminal = {
+
+        win = {
+          -- height = 0.2,
+        },
+      },
+    },
+  },
+}
