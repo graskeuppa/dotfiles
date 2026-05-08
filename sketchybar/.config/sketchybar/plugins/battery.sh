32a46a4a64c0749b686d33fe091e5bea58961375
gras <birdlandbollas@ciencias.unam.mx>
2026-05-07 22:58:45 -0600

    added logic for ac power

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-HOME= "/Users/gras"
+HOME="/Users/gras"
 source "$HOME/.config/sketchybar/colors_catppuccinmocha.sh"
 source "$HOME/.config/sketchybar/icons.sh"
 
 PERCENTAGE="$(pmset -g batt | grep -Eo "\d+%" | cut -d% -f1)"
-CHARGING="$(pmset -g batt | grep 'AC Power')"
+BATT_STATUS="$(pmset -g batt)"
+CHARGING="$(echo "$BATT_STATUS" | grep 'AC Power')"
 
 if [ "$PERCENTAGE" = "" ]; then
   exit 0
@@ -13,42 +14,40 @@ fi
 
 DRAWING=on
 COLOR=$TEXT
+
+# --- Existing Percentage Logic ---
 case "${PERCENTAGE}" in
 9[0-9] | 100)
   ICON=$BATTERY_100
-  DRAWING=on
   ;;
 [6-8][0-9])
   ICON=$BATTERY_75
-  DRAWING=on
   ;;
 [3-5][0-9])
   ICON=$BATTERY_50
-  DRAWING=on
   ;;
 2[0-9])
   ICON=$BATTERY_25
   COLOR=$PEACH
-  DRAWING=on
   ;;
 [0-1][0-9])
   ICON=$BATTERY_25
   COLOR=$RED
-  DRAWING=on
   ;;
 *)
   ICON=$BATTERY_0
   COLOR=$RED
-  DRAWING=on
   ;;
 esac
 
 if [[ "$CHARGING" != "" ]]; then
-  ICON=$BATTERY_CHARGING
-  DRAWING=on
-  COLOR=$TEAL
+  if echo "$BATT_STATUS" | grep -q "not charging"; then
+    ICON=$AC_POWER
+    COLOR=$TEAL
+  else
+    ICON=$BATTERY_CHARGING
+    COLOR=$MAUVE
+  fi
 fi
 
-# The item invoking this script (name $NAME) will get its icon and label
-# updated with the current battery
 sketchybar --animate tanh 60 --set "$NAME" drawing="$DRAWING" icon="$ICON" icon.color="$COLOR"
